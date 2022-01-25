d2add391a790ac02146f444d0af44e117cf0b622
Ahmed Khalaf <pxlshpr@gmail.com>
2022-01-25 08:52:23 +0500

    add public init

--- Sources/SwiftUISugar/CustomCorner.swift
+++ Sources/SwiftUISugar/CustomCorner.swift
@@ -1,8 +1,13 @@
 import SwiftUI
 
 public struct CustomCorner: Shape {
-    public var corners: UIRectCorner
-    public var radius: CGFloat
+    var corners: UIRectCorner
+    var radius: CGFloat
+    
+    public init(corners: UIRectCorner, radius: CGFloat) {
+        self.corners = corners
+        self.radius = radius
+    }
     
     public func path(in rect: CGRect) -> Path {
         let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
