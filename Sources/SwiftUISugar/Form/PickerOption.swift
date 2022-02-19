d086e18b477598e0f623a5931d45aa6beb8b2292
Ahmed Khalaf <pxlshpr@gmail.com>
2022-02-19 16:24:07 +0500

    bump

--- Sources/SwiftUISugar/Form/PickerOption.swift
+++ Sources/SwiftUISugar/Form/PickerOption.swift
@@ -1,29 +1,10 @@
 import Foundation
 
 public protocol StringsProvider {
-    func title(for unit: PickerOption, isPlural: Bool) -> String
-    func subtitle(for unit: PickerOption, isPlural: Bool) -> String?
+    func title(for option: PickerOption, isPlural: Bool) -> String
+    func subtitle(for option: PickerOption, isPlural: Bool) -> String?
 }
 
 public protocol PickerOption {
     var optionId: String { get }
-//    func title(isPlural: Bool) -> String
-//    func subtitle(isPlural: Bool) -> String?
 }
-
-//public extension PickerOption {
-//    func subtitle(isPlural: Bool) -> String? {
-//        return nil
-//    }
-//}
-
-//public extension PickerOption {
-//    func title(for value: Double?) -> String {
-//        guard let value = value else { return title(isPlural: false) }
-//        return title(isPlural: value > 1)
-//    }
-//    func subtitle(for value: Double?) -> String? {
-//        guard let value = value else { return subtitle(isPlural: false) }
-//        return subtitle(isPlural: value > 1)
-//    }
-//}
