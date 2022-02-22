4feb5c51e4d4ffa1fae1822776b39b5421bb8b2a
Ahmed Khalaf <pxlshpr@gmail.com>
2022-02-22 13:04:54 +0500

    bump

---
+++ Sources/SwiftUISugar/Form/EmojiTextField.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+
+/// Based off answer in: https://stackoverflow.com/a/66397959
+class UIEmojiTextField: UITextField {
+    
+    /// Amended using: https://stackoverflow.com/a/69760106
+    override var textInputMode: UITextInputMode? {
+        .activeInputModes.first(where: { $0.primaryLanguage == "emoji" })
+    }
+}
+
+struct EmojiTextField: UIViewRepresentable {
+    @Binding var text: String
+    var placeholder: String = ""
+    
+    func makeUIView(context: Context) -> UIEmojiTextField {
+        let emojiTextField = UIEmojiTextField()
+        emojiTextField.placeholder = placeholder
+        emojiTextField.text = text
+        emojiTextField.delegate = context.coordinator
+        return emojiTextField
+    }
+    
+    func updateUIView(_ uiView: UIEmojiTextField, context: Context) {
+        uiView.text = text
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    class Coordinator: NSObject, UITextFieldDelegate {
+        var parent: EmojiTextField
+        
+        init(parent: EmojiTextField) {
+            self.parent = parent
+        }
+        
+        func textFieldDidChangeSelection(_ textField: UITextField) {
+            DispatchQueue.main.async { [weak self] in
+                self?.parent.text = textField.text ?? ""
+            }
+        }
+    }
+}
