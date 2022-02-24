6e238f9eb53f592cf45dbdfe6ed6221b14c82e52
Ahmed Khalaf <pxlshpr@gmail.com>
2022-02-25 01:51:43 +0500

    bump

--- Sources/SwiftUISugar/Form/Field.swift
+++ Sources/SwiftUISugar/Form/Field.swift
@@ -9,30 +9,22 @@ public enum SelectorStyle {
 }
 
 public struct Field: View {
-    
-    @Binding var label: String
-    @Binding var value: String
-    @State var placeholder: String? = nil
-    @State var keyboardType: UIKeyboardType
 
-    /// Single (optional) unit
-    @State var unit: String? = nil
-    
+    @Binding var label: String
+    var value: Binding<String>?
+    @State var keyboardType: UIKeyboardType = .default
+    var stylingProvider: StylingProvider?
+
+    @State var placeholder: String? = nil
+    @State var unit: String? = nil /// Single (optional) unit
+
     /// Multiple units
     var units: Binding<[SelectionOption]>? = nil
     var selectedUnit: Binding<SelectionOption>? = nil
-    var customUnitString: Binding<String>? = nil
-    @State var showPickerOnAppear: Bool = false
-    var customIsShowingPicker: Binding<Bool>? = nil
     var onUnitChanged: UnitChangedHandler? = nil
 
-    @State private var showingActionSheet: Bool = false
     @FocusState private var isFocused: Bool
 
-    @State var selectorStyle: SelectorStyle
-
-    var stylingProvider: StylingProvider?
-    
     //MARK: - Initializers
     public init(
         label: Binding<String>,
@@ -40,63 +32,61 @@ public struct Field: View {
         placeholder: String? = nil,
         unit: String? = nil,
         keyboardType: UIKeyboardType = .alphabet,
-        showPickerOnAppear: Bool = false,
-        isShowingPicker: Binding<Bool>? = nil,
-        selectorStyle: SelectorStyle = .menu,
         stylingProvider: StylingProvider? = nil,
         onUnitChanged: UnitChangedHandler? = nil
     ) {
         self._label = label
-        self._value = value
+        self.value = value
         self._keyboardType = State(initialValue: keyboardType)
-        self._showPickerOnAppear = State(initialValue: showPickerOnAppear)
-        self.customIsShowingPicker = isShowingPicker
         self._placeholder = State(initialValue: placeholder)
         self._unit = State(initialValue: unit)
         self.onUnitChanged = onUnitChanged
-        self._selectorStyle = State(initialValue: selectorStyle)
         self.stylingProvider = stylingProvider
     }
-    
+
     public init(
         label: Binding<String>,
         value: Binding<String>,
         placeholder: String? = nil,
         units: Binding<[SelectionOption]>,
         selectedUnit: Binding<SelectionOption>,
-        customUnitString: Binding<String>? = nil,
         keyboardType: UIKeyboardType = .alphabet,
-        showPickerOnAppear: Bool = false,
-        isShowingPicker: Binding<Bool>? = nil,
-        selectorStyle: SelectorStyle = .menu,
         stylingProvider: StylingProvider? = nil,
         onUnitChanged: UnitChangedHandler? = nil
     ) {
         self._label = label
-        self._value = value
+        self.value = value
         self._keyboardType = State(initialValue: keyboardType)
-        self._showPickerOnAppear = State(initialValue: showPickerOnAppear)
-        self.customIsShowingPicker = isShowingPicker
         self._placeholder = State(initialValue: placeholder)
         self.units = units
         self.selectedUnit = selectedUnit
-        self.customUnitString = customUnitString
         self.onUnitChanged = onUnitChanged
-        self._selectorStyle = State(initialValue: selectorStyle)
+        self.stylingProvider = stylingProvider
+    }
+
+    public init(
+        label: String,
+        units: Binding<[SelectionOption]>,
+        selectedUnit: Binding<SelectionOption>,
+        stylingProvider: StylingProvider? = nil,
+        onUnitChanged: UnitChangedHandler? = nil
+    ) {
+        self._label = .constant(label)
+        self.value = nil
+        self.units = units
+        self.selectedUnit = selectedUnit
+        self.onUnitChanged = onUnitChanged
         self.stylingProvider = stylingProvider
     }
     
     //MARK: Convenience Initializers
-    
+
     public init(
         label: String,
         value: Binding<String>,
         placeholder: String? = nil,
         unit: String? = nil,
         keyboardType: UIKeyboardType = .alphabet,
-        showPickerOnAppear: Bool = false,
-        isShowingPicker: Binding<Bool>? = nil,
-        selectorStyle: SelectorStyle = .menu,
         stylingProvider: StylingProvider? = nil,
         onUnitChanged: UnitChangedHandler? = nil
     ) {
@@ -106,24 +96,17 @@ public struct Field: View {
             placeholder: placeholder,
             unit: unit,
             keyboardType: keyboardType,
-            showPickerOnAppear: showPickerOnAppear,
-            isShowingPicker: isShowingPicker,
-            selectorStyle: selectorStyle,
             stylingProvider: stylingProvider,
             onUnitChanged: onUnitChanged)
     }
-    
+
     public init(
         label: String,
         value: Binding<String>,
         placeholder: String? = nil,
         units: Binding<[SelectionOption]>,
         selectedUnit: Binding<SelectionOption>,
-        customUnitString: Binding<String>? = nil,
         keyboardType: UIKeyboardType = .alphabet,
-        showPickerOnAppear: Bool = false,
-        isShowingPicker: Binding<Bool>? = nil,
-        selectorStyle: SelectorStyle = .menu,
         stylingProvider: StylingProvider? = nil,
         onUnitChanged: UnitChangedHandler? = nil
     ) {
@@ -133,28 +116,19 @@ public struct Field: View {
             placeholder: placeholder,
             units: units,
             selectedUnit: selectedUnit,
-            customUnitString: customUnitString,
             keyboardType: keyboardType,
-            showPickerOnAppear: showPickerOnAppear,
-            isShowingPicker: isShowingPicker,
-            selectorStyle: selectorStyle,
             stylingProvider: stylingProvider,
             onUnitChanged: onUnitChanged)
     }
 
     public var body: some View {
         if let units = units {
-            switch selectorStyle {
-            case .actionSheet:
-                fieldWithPicker(for: units)
-            case .menu:
-                fieldWithMenu(for: units)
-            }
+            fieldWithMenu(for: units)
         } else {
             field
         }
     }
-    
+
     var field: some View {
         ZStack {
             labelsLayer
@@ -165,37 +139,14 @@ public struct Field: View {
         }
     }
 
-    func fieldWithPicker(for units: Binding<[SelectionOption]>) -> some View {
-        HStack {
-            field
-            unitText
-                .onTapGesture {
-                    if units.count > 1 {
-                        Haptics.feedback(style: .soft)
-                        showingActionSheet = true
-                        customIsShowingPicker?.wrappedValue = true
-                    }
-                }
-        }
-        .if(customIsShowingPicker != nil) { view in
-            view
-                .actionSheet(isPresented: customIsShowingPicker!) { actionSheet(for: units) }
-        }
-        .if(customIsShowingPicker == nil) { view in
-            view
-                .actionSheet(isPresented: $showingActionSheet) { actionSheet(for: units) }
-        }
-        .onChange(of: units.count) { newValue in
-            if showPickerOnAppear && units.wrappedValue.count > 1 {
-                showingActionSheet = true
-                showPickerOnAppear = false
-            }
-        }
-    }
-    
     func fieldWithMenu(for units: Binding<[SelectionOption]>) -> some View {
         HStack {
-            field
+            if value != nil {
+                field
+            } else {
+                labelsLayer
+                Spacer()
+            }
             if units.count > 1 {
                 menu(for: units)
             } else {
@@ -203,7 +154,7 @@ public struct Field: View {
             }
         }
     }
-    
+
     func menu(for options: Binding<[SelectionOption]>) -> some View {
         Menu {
             ForEach(options.indices, id: \.self) { index in
@@ -225,7 +176,7 @@ public struct Field: View {
             Haptics.feedback(style: .soft)
         }
     }
-    
+
     @ViewBuilder
     func secondaryMenu(for options: [SelectionOption], option: SelectionOption) -> some View {
         Menu {
@@ -252,11 +203,11 @@ public struct Field: View {
             Haptics.feedback(style: .soft)
         }
     }
-    
+
     func title(for option: SelectionOption) -> String {
         stylingProvider?.title(for: option, isPlural: false) ?? "Unsupported Option"
     }
-    
+
     @ViewBuilder
     func tertiaryMenu(for options: [SelectionOption], option: SelectionOption) -> some View {
         Menu {
@@ -279,8 +230,8 @@ public struct Field: View {
             Haptics.feedback(style: .soft)
         }
     }
-    
-    
+
+
     func menuButton(for option: SelectionOption) -> some View {
         Button(action: {
             selectedUnit?.wrappedValue = option
@@ -293,20 +244,20 @@ public struct Field: View {
             }
         }
     }
-    
+
     //MARK: - Option Button
     let FontSize = 13.0
     let FontWeight: Font.Weight = .semibold
     let UIFontWeight: UIFont.Weight = .semibold
     static var PaddingTapTargetHorizontal: CGFloat = 10.0
     static var PaddingTapTargetVertical: CGFloat = 7.0
-    
+
     @Environment(\.colorScheme) var colorScheme
 
     @ViewBuilder
     func unitButtonText(forSingleUnit: Bool = false) -> some View {
         HStack(spacing: 0) {
-            VStack (alignment: .leading) {
+            VStack (alignment: value == nil ? .trailing : .leading) {
                 Text(selectedUnitString)
                     .font(.headline)
                     .multilineTextAlignment(.leading)
@@ -334,7 +285,7 @@ public struct Field: View {
         .grayscale(forSingleUnit ? 1.0 : 0.0)
         .disabled(forSingleUnit)
     }
-    
+
     @ViewBuilder
     var backgroundView: some View {
         RoundedRectangle(cornerRadius: 6)
@@ -370,63 +321,70 @@ public struct Field: View {
                 .animation(.interactiveSpring(), value: selectedUnitString)
         }
     }
-    
+
     var subtitle: String? {
         guard let unit = selectedUnit?.wrappedValue else { return nil }
         return stylingProvider?.subtitle(for: unit, isPlural: isPlural)
     }
-    
+
     var isPlural: Bool {
-        guard let double = Double(value) else { return false }
+        guard let value = value?.wrappedValue, let double = Double(value) else { return false }
         return double > 1
     }
-    
+
     var unitTextColor: Color {
         guard let units = units?.wrappedValue else {
             return Color(.secondaryLabel)
         }
         return units.count > 1 ? Color.accentColor : Color(.secondaryLabel)
     }
-    
+
     @ViewBuilder
     var textField: some View {
-        TextField(placeholder ?? "", text: $value)
-            .focused($isFocused)
-            .keyboardType(keyboardType)
-            .multilineTextAlignment(.trailing)
-            .frame(maxHeight: .infinity)
+        if let value = value {
+            TextField(placeholder ?? "", text: value)
+                .focused($isFocused)
+                .keyboardType(keyboardType)
+                .multilineTextAlignment(.trailing)
+                .frame(maxHeight: .infinity)
+        }
     }
-    
+
     @ViewBuilder
     var textFieldLayer: some View {
         HStack {
             Spacer()
                 .frame(width: label.widthForLabelFont + Padding)
             textField
-            if let units = unit {
-                Text(units)
-                    .foregroundColor(Color(.clear))
-                    .multilineTextAlignment(.trailing)
-            }
+//            if let units = unit {
+//                Text(units)
+//                    .foregroundColor(Color(.clear))
+//                    .multilineTextAlignment(.trailing)
+//            }
         }
     }
-    
+
     @ViewBuilder
     var labelsLayer: some View {
-        HStack {
-            Text(label)
-                .foregroundColor($value.wrappedValue.count > 0 ? Color(.secondaryLabel) : Color(.tertiaryLabel))
-            Spacer()
-            if let units = unit {
-                Text(units)
-                    .foregroundColor($value.wrappedValue.count > 0 ? Color(.secondaryLabel) : Color(.tertiaryLabel))
-                    .multilineTextAlignment(.trailing)
+        if let value = value {
+            HStack {
+                Text(label)
+                    .foregroundColor(value.wrappedValue.count > 0 ? Color(.secondaryLabel) : Color(.tertiaryLabel))
+                Spacer()
+//                if let units = unit {
+//                    Text(units)
+//                        .foregroundColor(value.wrappedValue.count > 0 ? Color(.secondaryLabel) : Color(.tertiaryLabel))
+//                        .multilineTextAlignment(.trailing)
+//                }
             }
+        } else {
+            Text(label)
+                .foregroundColor(Color(.label))
         }
     }
-    
+
     //MARK: Action Sheet
-    
+
     func actionSheet(for units: Binding<[SelectionOption]>) -> ActionSheet {
         ActionSheet(title: Text("Units"), buttons: actionSheetButtons(for: units))
     }
@@ -439,36 +397,33 @@ public struct Field: View {
         buttons.append(.cancel())
         return buttons
     }
-    
+
     func buttonFor(_ unit: SelectionOption) -> ActionSheet.Button {
         return ActionSheet.Button.default(Text(unitString(for: unit)), action: {
             selectedUnit?.wrappedValue = unit
             onUnitChanged?(unit)
         })
     }
-    
+
     //MARK: - Helpers
-    
+
     var selectedUnitString: String {
         guard let selectedUnit = selectedUnit?.wrappedValue else {
             return ""
         }
-        if let customUnitString = customUnitString?.wrappedValue {
-            return customUnitString
-        } else {
-            return unitString(for: selectedUnit)
-        }
+        return unitString(for: selectedUnit)
     }
 
     func unitString(for unit: SelectionOption?) -> String {
         guard let unit = unit, let stylingProvider = stylingProvider else {
             return ""
         }
-        guard let value = Double(value) else {
+        if let value = value, let doubleValue = Double(value.wrappedValue) {
+            return stylingProvider.title(for: unit, isPlural: doubleValue > 1)
+        } else {
             return stylingProvider.title(for: unit, isPlural: false)
         }
-        return stylingProvider.title(for: unit, isPlural: value > 1)
     }
-    
+
     let Padding: CGFloat = 10.0
 }
