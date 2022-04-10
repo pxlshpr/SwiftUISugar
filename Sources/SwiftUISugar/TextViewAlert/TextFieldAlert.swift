a93f9ad7f1428606eebbd3b402929567074dd0af
Ahmed Khalaf <pxlshpr@gmail.com>
2022-04-10 11:59:29 +0500

    tag increment

--- Sources/SwiftUISugar/TextViewAlert/TextFieldAlert.swift
+++ Sources/SwiftUISugar/TextViewAlert/TextFieldAlert.swift
@@ -7,19 +7,21 @@ public struct TextFieldAlert {
     // MARK: Properties
     let title: String
     let message: String?
+    var actions: [UIAlertAction]? = nil
     @Binding var text: String?
     var isPresented: Binding<Bool>? = nil
     
-    public init(title: String, message: String? = nil, text: Binding<String?>, isPresented: Binding<Bool>? = nil) {
+    public init(title: String, message: String? = nil, actions: [UIAlertAction]? = nil, text: Binding<String?>, isPresented: Binding<Bool>? = nil) {
         self.title = title
         self.message = message
+        self.actions = actions
         self._text = text
         self.isPresented = isPresented
     }
     
     // MARK: Modifiers
     func dismissable(_ isPresented: Binding<Bool>) -> TextFieldAlert {
-        TextFieldAlert(title: title, message: message, text: $text, isPresented: isPresented)
+        TextFieldAlert(title: title, message: message, actions: actions, text: $text, isPresented: isPresented)
     }
 }
 
@@ -28,7 +30,7 @@ extension TextFieldAlert: UIViewControllerRepresentable {
     public typealias UIViewControllerType = TextFieldAlertViewController
     
     public func makeUIViewController(context: UIViewControllerRepresentableContext<TextFieldAlert>) -> UIViewControllerType {
-        TextFieldAlertViewController(title: title, message: message, text: $text, isPresented: isPresented)
+        TextFieldAlertViewController(title: title, message: message, actions: actions, text: $text, isPresented: isPresented)
     }
     
     public func updateUIViewController(_ uiViewController: UIViewControllerType,
--- Sources/SwiftUISugar/TextViewAlert/TextFieldAlertViewController.swift
+++ Sources/SwiftUISugar/TextViewAlert/TextFieldAlertViewController.swift
@@ -10,9 +10,10 @@ public class TextFieldAlertViewController: UIViewController {
     ///   - message: to be used as optional message of the UIAlertController
     ///   - text: binding for the text typed into the UITextField
     ///   - isPresented: binding to be set to false when the alert is dismissed (`Done` button tapped)
-    init(title: String, message: String?, text: Binding<String?>, isPresented: Binding<Bool>?) {
+    init(title: String, message: String?, actions: [UIAlertAction]? = nil, text: Binding<String?>, isPresented: Binding<Bool>?) {
         self.alertTitle = title
         self.message = message
+        self.actions = actions
         self._text = text
         self.isPresented = isPresented
         super.init(nibName: nil, bundle: nil)
@@ -25,6 +26,7 @@ public class TextFieldAlertViewController: UIViewController {
     // MARK: - Dependencies
     private let alertTitle: String
     private let message: String?
+    private let actions: [UIAlertAction]?
     @Binding private var text: String?
     private var isPresented: Binding<Bool>?
     
@@ -42,7 +44,7 @@ public class TextFieldAlertViewController: UIViewController {
         
         let vc = UIAlertController(title: alertTitle, message: message, preferredStyle: .alert)
         
-        // add a textField and create a subscription to update the `text` binding
+        /// Add a textField and create a subscription to update the `text` binding
         vc.addTextField { [weak self] textField in
             guard let self = self else { return }
             textField.text = self.text
@@ -54,13 +56,18 @@ public class TextFieldAlertViewController: UIViewController {
                 .assign(to: \.text, on: self)
         }
         
-        // create a `Done` action that updates the `isPresented` binding when tapped
-        // this is just for Demo only but we should really inject
-        // an array of buttons (with their title, style and tap handler)
-        let action = UIAlertAction(title: "Done", style: .default) { [weak self] _ in
-            self?.isPresented?.wrappedValue = false
+        /// Add Actions
+        if let actions = actions, actions.count > 0 {
+            actions.forEach { action in
+                vc.addAction(action)
+            }
+        } else {
+            let action = UIAlertAction(title: "Done", style: .default) { [weak self] _ in
+                self?.isPresented?.wrappedValue = false
+            }
+            vc.addAction(action)
         }
-        vc.addAction(action)
+        
         present(vc, animated: true, completion: nil)
     }
 }
