5acb227aa81b50472a94b07df987678a2a3bc872
Ahmed Khalaf <pxlshpr@gmail.com>
2022-07-05 18:35:16 +0400

    tag increment

--- Sources/SwiftUISugar/Form/Field/Field+Menu.swift
+++ Sources/SwiftUISugar/Form/Field/Field+Menu.swift
@@ -2,129 +2,9 @@ import SwiftUI
 import SwiftHaptics
 
 extension Field {
-    
-    static func title(for option: SelectionOption?, forMenu: Bool = false, value: Binding<String>?, contentProvider: FieldContentProvider?) -> String {
-        guard let option = option else { return "Unsupported" }
-        var isPlural = true
-        if let value = value, let doubleValue = Double(value.wrappedValue) {
-            isPlural = doubleValue > 1
-        }
-        if forMenu {
-            return contentProvider?.menuTitle(for: option, isPlural: isPlural)
-            ?? option.menuTitle(isPlural: isPlural)
-            ?? contentProvider?.title(for: option, isPlural: isPlural)
-            ?? option.title(isPlural: isPlural) ?? "Unsupported"
-        } else {
-            return contentProvider?.title(for: option, isPlural: isPlural) ?? option.title(isPlural: isPlural) ?? "Unsupported"
-        }
-    }
-
-    @ViewBuilder
-    static func menuButtonLabel(for option: SelectionOption, value: Binding<String>?, contentProvider: FieldContentProvider?) -> some View {
-        let title = title(for: option, forMenu: true, value: value, contentProvider: contentProvider)
-        if let accessorySystemImage = contentProvider?.accessorySystemImage(for: option) {
-            Label(title, systemImage: accessorySystemImage)
-        } else if let accessorySystemImage = option.accessorySystemImage {
-            Label(title, systemImage: accessorySystemImage)
-        } else {
-            Text(title)
-        }
-    }
-    
-    @ViewBuilder
-    static func menuButton(for option: SelectionOption, value: Binding<String>?, contentProvider: FieldContentProvider?, onOptionSelected: @escaping ((SelectionOption) -> Void)) -> some View {
-        Button(role: option.role) {
-            onOptionSelected(option)
-            NotificationCenter.default.post(name: .selectionOptionChanged, object: nil)
-        } label: {
-            menuButtonLabel(for: option, value: value, contentProvider: contentProvider)
-        }
-//        Button(action: {
-//            onOptionSelected(option)
-//            NotificationCenter.default.post(name: .selectionOptionChanged, object: nil)
-//        }) {
-//            menuButtonLabel(for: option, value: value, contentProvider: contentProvider)
-//        }
-    }
-    
-    @ViewBuilder
-    public static func menu<LabelContent: View>(for options: [SelectionOption], label: LabelContent, value: Binding<String>?, contentProvider: FieldContentProvider? = nil, onOptionSelection: @escaping ((SelectionOption) -> Void)) -> some View {
-        Menu {
-            ForEach(options.indices, id: \.self) { index in
-                let option = options[index]
-                if let _ = option as? SelectionDivider {
-                    Divider()
-                } else if option.isGroup, let subOptions = option.subOptions {
-                    secondaryMenu(
-                        for: subOptions,
-                        label: menuButton(for: option, value: value, contentProvider: contentProvider, onOptionSelected: onOptionSelection),
-                        value: value,
-                        contentProvider: contentProvider,
-                        onOptionSelection: onOptionSelection
-                    )
-                } else {
-                    menuButton(for: option, value: value, contentProvider: contentProvider, onOptionSelected: onOptionSelection)
-                }
-            }
-        } label: {
-            label
-        }
-        .onTapGesture {
-            Haptics.feedback(style: .soft)
-        }
-    }
-    
-    @ViewBuilder
-    static func secondaryMenu<LabelContent: View>(for options: [SelectionOption], label: LabelContent, value: Binding<String>?, contentProvider: FieldContentProvider?, onOptionSelection: @escaping ((SelectionOption) -> Void)) -> some View {
-        Menu {
-            ForEach(options.indices, id: \.self) { index in
-                let option = options[index]
-                if let _ = option as? SelectionDivider {
-                    Divider()
-                } else if option.isGroup, let subOptions = option.subOptions {
-                    tertiaryMenu(
-                        for: subOptions,
-                        label: menuButton(for: option, value: value, contentProvider: contentProvider, onOptionSelected: onOptionSelection),
-                        value: value,
-                        contentProvider: contentProvider,
-                        onOptionSelection: onOptionSelection
-                    )
-                } else {
-                    menuButton(for: option, value: value, contentProvider: contentProvider, onOptionSelected: onOptionSelection)
-                }
-            }
-        } label: {
-            label
-        }
-        .onTapGesture {
-            Haptics.feedback(style: .soft)
-        }
-    }
-
-    @ViewBuilder
-    static func tertiaryMenu<LabelContent: View>(for options: [SelectionOption], label: LabelContent, value: Binding<String>?, contentProvider: FieldContentProvider?, onOptionSelection: @escaping ((SelectionOption) -> Void)) -> some View {
-        Menu {
-            ForEach(options.indices, id: \.self) { index in
-                let option = options[index]
-                if let _ = option as? SelectionDivider {
-                    Divider()
-                } else {
-                    menuButton(for: option, value: value, contentProvider: contentProvider, onOptionSelected: onOptionSelection)
-                }
-            }
-        } label: {
-            label
-        }
-        .onTapGesture {
-            Haptics.feedback(style: .soft)
-        }
-    }
-}
-
-extension Field {
     @ViewBuilder
     func menu(for options: Binding<[SelectionOption]>) -> some View {
-        Self.menu(
+        fieldMenu(
             for: options.wrappedValue,
             label: selectedOptionText,
             value: value,
@@ -135,90 +15,125 @@ extension Field {
     }
 }
 
-//extension Field {
-//
-//    @ViewBuilder
-//    func menu(for options: Binding<[SelectionOption]>) -> some View {
-//        Menu {
-//            ForEach(options.indices, id: \.self) { index in
-//                let option = options.wrappedValue[index]
-//                if let _ = option as? SelectionDivider {
-//                    Divider()
-//                } else if option.isGroup, let subOptions = option.subOptions {
-//                    secondaryMenu(for: subOptions, option: option)
-//                } else {
-//                    menuButton(for: option)
-//                }
-//            }
-//        } label: {
-//            selectedOptionText
-//        }
-//        .onTapGesture {
-//            Haptics.feedback(style: .soft)
-//        }
-//    }
-//
-//    @ViewBuilder
-//    func secondaryMenu(for options: [SelectionOption], option: SelectionOption) -> some View {
-//        Menu {
-//            ForEach(options.indices, id: \.self) { index in
-//                let option = options[index]
-//                if let _ = option as? SelectionDivider {
-//                    Divider()
-//                } else if option.isGroup, let subOptions = option.subOptions {
-//                    tertiaryMenu(for: subOptions, option: option)
-//                } else {
-//                    menuButton(for: option)
-//                }
-//            }
-//        } label: {
-//            menuButtonLabel(for: option)
-//        }
-//        .onTapGesture {
-//            Haptics.feedback(style: .soft)
-//        }
-//    }
-//
-//    @ViewBuilder
-//    func tertiaryMenu(for options: [SelectionOption], option: SelectionOption) -> some View {
-//        Menu {
-//            ForEach(options.indices, id: \.self) { index in
-//                let option = options[index]
-//                if let _ = option as? SelectionDivider {
-//                    Divider()
-//                } else {
-//                    menuButton(for: option)
-//                }
-//            }
-//        } label: {
-//            menuButtonLabel(for: option)
-//        }
-//        .onTapGesture {
-//            Haptics.feedback(style: .soft)
-//        }
-//    }
-//
-//    //MARK: - Helpers
-//
-//    @ViewBuilder
-//    func menuButtonLabel(for option: SelectionOption) -> some View {
-//        let title = title(for: option, forMenu: true)
-//        if let systemImage = contentProvider?.systemImage(for: option) {
-//            Label(title, systemImage: systemImage)
-//        } else if let systemImage = option.systemImage {
-//            Label(title, systemImage: systemImage)
-//        } else {
-//            Text(title)
-//        }
-//    }
-//
-//    @ViewBuilder
-//    func menuButton(for option: SelectionOption) -> some View {
+//MARK: - Global Functions
+@ViewBuilder
+public func fieldMenu<LabelContent: View>(
+    for options: [SelectionOption],
+    label: LabelContent,
+    value: Binding<String>?,
+    contentProvider: FieldContentProvider? = nil,
+    onOptionSelection: @escaping ((SelectionOption) -> Void)
+) -> some View {
+    Menu {
+        ForEach(options.indices, id: \.self) { index in
+            let option = options[index]
+            if let _ = option as? SelectionDivider {
+                Divider()
+            } else if option.isGroup, let subOptions = option.subOptions {
+                fieldSecondaryMenu(
+                    for: subOptions,
+                    label: fieldMenuButton(for: option, value: value, contentProvider: contentProvider, onOptionSelected: onOptionSelection),
+                    value: value,
+                    contentProvider: contentProvider,
+                    onOptionSelection: onOptionSelection
+                )
+            } else {
+                fieldMenuButton(for: option, value: value, contentProvider: contentProvider, onOptionSelected: onOptionSelection)
+            }
+        }
+    } label: {
+        label
+    }
+    .onTapGesture {
+        Haptics.feedback(style: .soft)
+    }
+}
+
+@ViewBuilder
+func fieldSecondaryMenu<LabelContent: View>(for options: [SelectionOption], label: LabelContent, value: Binding<String>?, contentProvider: FieldContentProvider?, onOptionSelection: @escaping ((SelectionOption) -> Void)) -> some View {
+    Menu {
+        ForEach(options.indices, id: \.self) { index in
+            let option = options[index]
+            if let _ = option as? SelectionDivider {
+                Divider()
+            } else if option.isGroup, let subOptions = option.subOptions {
+                fieldTertiaryMenu(
+                    for: subOptions,
+                    label: fieldMenuButton(for: option, value: value, contentProvider: contentProvider, onOptionSelected: onOptionSelection),
+                    value: value,
+                    contentProvider: contentProvider,
+                    onOptionSelection: onOptionSelection
+                )
+            } else {
+                fieldMenuButton(for: option, value: value, contentProvider: contentProvider, onOptionSelected: onOptionSelection)
+            }
+        }
+    } label: {
+        label
+    }
+    .onTapGesture {
+        Haptics.feedback(style: .soft)
+    }
+}
+
+@ViewBuilder
+func fieldTertiaryMenu<LabelContent: View>(for options: [SelectionOption], label: LabelContent, value: Binding<String>?, contentProvider: FieldContentProvider?, onOptionSelection: @escaping ((SelectionOption) -> Void)) -> some View {
+    Menu {
+        ForEach(options.indices, id: \.self) { index in
+            let option = options[index]
+            if let _ = option as? SelectionDivider {
+                Divider()
+            } else {
+                fieldMenuButton(for: option, value: value, contentProvider: contentProvider, onOptionSelected: onOptionSelection)
+            }
+        }
+    } label: {
+        label
+    }
+    .onTapGesture {
+        Haptics.feedback(style: .soft)
+    }
+}
+
+func fieldMenuTitle(for option: SelectionOption?, forMenu: Bool = false, value: Binding<String>?, contentProvider: FieldContentProvider?) -> String {
+    guard let option = option else { return "Unsupported" }
+    var isPlural = true
+    if let value = value, let doubleValue = Double(value.wrappedValue) {
+        isPlural = doubleValue > 1
+    }
+    if forMenu {
+        return contentProvider?.menuTitle(for: option, isPlural: isPlural)
+        ?? option.menuTitle(isPlural: isPlural)
+        ?? contentProvider?.title(for: option, isPlural: isPlural)
+        ?? option.title(isPlural: isPlural) ?? "Unsupported"
+    } else {
+        return contentProvider?.title(for: option, isPlural: isPlural) ?? option.title(isPlural: isPlural) ?? "Unsupported"
+    }
+}
+@ViewBuilder
+func fieldMenuButtonLabel(for option: SelectionOption, value: Binding<String>?, contentProvider: FieldContentProvider?) -> some View {
+    let title = fieldMenuTitle(for: option, forMenu: true, value: value, contentProvider: contentProvider)
+    if let accessorySystemImage = contentProvider?.accessorySystemImage(for: option) {
+        Label(title, systemImage: accessorySystemImage)
+    } else if let accessorySystemImage = option.accessorySystemImage {
+        Label(title, systemImage: accessorySystemImage)
+    } else {
+        Text(title)
+    }
+}
+
+@ViewBuilder
+func fieldMenuButton(for option: SelectionOption, value: Binding<String>?, contentProvider: FieldContentProvider?, onOptionSelected: @escaping ((SelectionOption) -> Void)) -> some View {
+    Button(role: option.role) {
+        onOptionSelected(option)
+        NotificationCenter.default.post(name: .selectionOptionChanged, object: nil)
+    } label: {
+        fieldMenuButtonLabel(for: option, value: value, contentProvider: contentProvider)
+    }
 //        Button(action: {
-//            selectedUnit?.wrappedValue = option
-//            onUnitChanged?(option)
+//            onOptionSelected(option)
+//            NotificationCenter.default.post(name: .selectionOptionChanged, object: nil)
 //        }) {
-//            menuButtonLabel(for: option)
+//            menuButtonLabel(for: option, value: value, contentProvider: contentProvider)
 //        }
-//    }
-//}
+}
