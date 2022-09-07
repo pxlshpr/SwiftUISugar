8aeb862458ac954c514698d2f852b5894d0d3e55
Ahmed Khalaf <pxlshpr@gmail.com>
2022-09-07 11:11:42 +0500

    tag increment

--- Sources/SwiftUISugar/Animations/NamespaceWrapper.swift
+++ Sources/SwiftUISugar/Animations/NamespaceWrapper.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
-class NamespaceWrapper: ObservableObject {
-    var namespace: Namespace.ID
+public class NamespaceWrapper: ObservableObject {
+    public var namespace: Namespace.ID
 
     init(_ namespace: Namespace.ID) {
         self.namespace = namespace
