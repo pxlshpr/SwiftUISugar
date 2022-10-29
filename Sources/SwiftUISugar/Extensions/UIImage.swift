f37e855b23aaef3a81a453de1beef27f19e93f00
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-29 12:44:08 +0500

    tag increment

--- Sources/SwiftUISugar/Extensions/UIImage.swift
+++ Sources/SwiftUISugar/Extensions/UIImage.swift
@@ -9,7 +9,11 @@ public extension UIImage {
 
         UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
         self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
-        let normalizedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        
+        guard let normalizedImage = UIGraphicsGetImageFromCurrentImageContext() else {
+            print("Unable to fix orientation of image")
+            return self
+        }
         UIGraphicsEndImageContext()
 
         return normalizedImage
