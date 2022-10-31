f03a0f7e5870c576403d8cdfd5ffe2abd83d8698
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-31 20:35:20 +0500

    tag increment

--- Sources/SwiftUISugar/EnvironmentKeys/NamespaceEnvironmentKey.swift
+++ Sources/SwiftUISugar/EnvironmentKeys/NamespaceEnvironmentKey.swift
@@ -1,20 +1,20 @@
-#if canImport(UIKit)
-import SwiftUI
-
-struct NamespaceEnvironmentKey: EnvironmentKey {
-    static var defaultValue: Namespace.ID = Namespace().wrappedValue
-}
-
-extension EnvironmentValues {
-    public var namespace: Namespace.ID {
-        get { self[NamespaceEnvironmentKey.self] }
-        set { self[NamespaceEnvironmentKey.self] = newValue }
-    }
-}
-
-extension View {
-    public func namespace(_ value: Namespace.ID) -> some View {
-        environment(\.namespace, value)
-    }
-}
-#endif
+//#if canImport(UIKit)
+//import SwiftUI
+//
+//struct NamespaceEnvironmentKey: EnvironmentKey {
+//    static var defaultValue: Namespace.ID = Namespace().wrappedValue
+//}
+//
+//extension EnvironmentValues {
+//    public var namespace: Namespace.ID {
+//        get { self[NamespaceEnvironmentKey.self] }
+//        set { self[NamespaceEnvironmentKey.self] = newValue }
+//    }
+//}
+//
+//extension View {
+//    public func namespace(_ value: Namespace.ID) -> some View {
+//        environment(\.namespace, value)
+//    }
+//}
+//#endif
