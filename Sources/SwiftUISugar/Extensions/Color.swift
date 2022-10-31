e5d7c119378d25b85eb065e85839d9eb5b90e7f9
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-31 16:07:07 +0500

    tag increment

--- Sources/SwiftUISugar/Extensions/Color.swift
+++ Sources/SwiftUISugar/Extensions/Color.swift
@@ -1,4 +1,5 @@
-//import SwiftUI
+import SwiftUI
+
 //
 //public extension Color {
 //    init(hex: String) {
@@ -26,3 +27,67 @@
 //        )
 //    }
 //}
+
+public extension Color {
+    func brighter(by amount: Double) -> Color {
+        Color(UIColor(self).adjustBrightness(by: amount))
+    }
+}
+
+import UIKit
+
+public extension UIColor {
+    /**
+     Create a ligher color
+     */
+//    func lighter(by percentage: CGFloat = 30.0) -> UIColor {
+//        return self.adjustBrightness(by: abs(percentage))
+//    }
+//
+//    /**
+//     Create a darker color
+//     */
+//    func darker(by percentage: CGFloat = 30.0) -> UIColor {
+//        return self.adjustBrightness(by: -abs(percentage))
+//    }
+    
+    /**
+     Try to increase brightness or decrease saturation
+     */
+    func adjustBrightness(by percentage: CGFloat = 30.0) -> UIColor {
+        var h: CGFloat = 0, s: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+        if self.getHue(&h, saturation: &s, brightness: &b, alpha: &a) {
+            if b < 1.0 {
+                let newB: CGFloat = max(min(b + (percentage/100.0)*b, 1.0), 0.0)
+                return UIColor(hue: h, saturation: s, brightness: newB, alpha: a)
+            } else {
+                let newS: CGFloat = min(max(s - (percentage/100.0)*s, 0.0), 1.0)
+                return UIColor(hue: h, saturation: newS, brightness: b, alpha: a)
+            }
+        }
+        return self
+    }
+}
+
+public extension UIColor {
+
+    func lighter(by percentage: CGFloat = 30.0) -> UIColor? {
+        return self.adjust(by: abs(percentage) )
+    }
+
+    func darker(by percentage: CGFloat = 30.0) -> UIColor? {
+        return self.adjust(by: -1 * abs(percentage) )
+    }
+
+    func adjust(by percentage: CGFloat = 30.0) -> UIColor? {
+        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
+        if self.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+            return UIColor(red: min(red + percentage/100, 1.0),
+                           green: min(green + percentage/100, 1.0),
+                           blue: min(blue + percentage/100, 1.0),
+                           alpha: alpha)
+        } else {
+            return nil
+        }
+    }
+}
