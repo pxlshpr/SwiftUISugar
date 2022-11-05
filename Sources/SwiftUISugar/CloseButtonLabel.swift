da52b5e9c1193bc2ab3f5fa215e0fca87a6004e0
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-05 23:41:00 +0500

    tag increment

--- Sources/SwiftUISugar/CloseButtonLabel.swift
+++ Sources/SwiftUISugar/CloseButtonLabel.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 public var closeButtonLabel: some View {
-    Image(systemName: "multiply.circle.fill")
+    Image(systemName: "xmark.circle.fill")
         .font(.system(size: 20))
         .symbolRenderingMode(.palette)
         .foregroundStyle(
