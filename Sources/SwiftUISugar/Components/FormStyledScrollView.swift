30d5773ab67bcfe67b431e8dae2629074de16e6f
Ahmed Khalaf <pxlshpr@gmail.com>
2022-12-25 23:31:47 +0500

    tag increment

--- Sources/SwiftUISugar/Components/FormStyledScrollView.swift
+++ Sources/SwiftUISugar/Components/FormStyledScrollView.swift
@@ -3,9 +3,11 @@ import SwiftUI
 
 public struct FormStyledScrollView<Content: View>: View {
     
+    let backgroundColor: Color
     var content: () -> Content
     
-    public init(@ViewBuilder content: @escaping () -> Content) {
+    public init(backgroundColor: Color = Color(.systemGroupedBackground), @ViewBuilder content: @escaping () -> Content) {
+        self.backgroundColor = backgroundColor
         self.content = content
     }
     
@@ -15,7 +17,7 @@ public struct FormStyledScrollView<Content: View>: View {
                 .frame(maxWidth: .infinity)
         }
         .background(
-            Color(.systemGroupedBackground)
+            backgroundColor
                 .edgesIgnoringSafeArea(.all) /// requireds to cover the area that would be covered by the keyboard during its dismissal animation
         )
     }
