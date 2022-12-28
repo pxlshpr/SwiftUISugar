1e8a4b45d7c0ad9aef3d157395ead5d8f34633c0
Ahmed Khalaf <pxlshpr@gmail.com>
2022-12-28 15:33:39 +0500

    tag increment

--- Sources/SwiftUISugar/DatePickerView.swift
+++ Sources/SwiftUISugar/DatePickerView.swift
@@ -16,6 +16,27 @@ public struct DatePickerView: View {
         self.didPickDate = didPickDate
     }
 
+    enum RelativeDay {
+        case today
+        case yesterday
+        case tomorrow
+        
+        var description: String {
+            switch self {
+            case .today:        return "Today"
+            case .yesterday:    return "Yesterday"
+            case .tomorrow:     return "Tomorrow"
+            }
+        }
+        
+        var todayOffset: Int {
+            switch self {
+            case .today:        return 0
+            case .yesterday:    return -1
+            case .tomorrow:     return 1
+            }
+        }
+    }
     public var body: some View {
         NavigationView {
             Form {
@@ -24,21 +45,38 @@ public struct DatePickerView: View {
             .navigationTitle(title)
             .navigationBarTitleDisplayMode(.inline)
             .toolbar { trailingContent }
+            .toolbar { leadingContent }
         }
     }
     
     var trailingContent: some ToolbarContent {
         ToolbarItem(placement: .navigationBarTrailing) {
-            if date.startOfDay != Date().startOfDay {
-                Button("Today") {
-                    Haptics.feedback(style: .soft)
-                    didPickDate(Date().startOfDay)
-                    dismiss()
-                }
+            if date.isToday || date.isYesterday {
+                button(relativeDay: .tomorrow)
+            } else {
+                button(relativeDay: .today)
             }
         }
     }
     
+    func button(relativeDay: RelativeDay) -> some View {
+        Button(relativeDay.description) {
+            Haptics.feedback(style: .soft)
+            didPickDate(Date().moveDayBy(relativeDay.todayOffset).startOfDay)
+            dismiss()
+        }
+    }
+
+    var leadingContent: some ToolbarContent {
+        ToolbarItem(placement: .navigationBarLeading) {
+            if date.isYesterday {
+                button(relativeDay: .today)
+            } else {
+                button(relativeDay: .yesterday)
+            }
+        }
+    }
+
     var title: String {
         "Pick a date"
     }
