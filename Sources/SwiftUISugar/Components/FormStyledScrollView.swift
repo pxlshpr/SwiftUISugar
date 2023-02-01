f2e3f40baec893e4b9ff5db531c941a840e578a0
Ahmed Khalaf <pxlshpr@gmail.com>
2023-02-01 14:03:44 +0500

    tag increment

--- Sources/SwiftUISugar/Components/FormStyledScrollView.swift
+++ Sources/SwiftUISugar/Components/FormStyledScrollView.swift
@@ -3,35 +3,24 @@ import SwiftUI
 
 public struct FormStyledScrollView<Content: View>: View {
     
-//    @Environment(\.colorScheme) var colorScheme
-    
-//    let customBackgroundColor: Color?
     var content: () -> Content
     
     let showsIndicators: Bool
     
     public init(showsIndicators: Bool = false, @ViewBuilder content: @escaping () -> Content) {
-//        self.customBackgroundColor = backgroundColor
         self.content = content
         self.showsIndicators = showsIndicators
     }
     
-//    var backgroundColor: Color {
-//        guard let customBackgroundColor else {
-////            return colorScheme == .dark ? Color(hex: "1C1C1E") : Color(hex: "F2F1F6")
-//            return colorScheme == .dark ? Color(hex: "1C1C1E") : Color(.systemGroupedBackground)
-//        }
-//        return customBackgroundColor
-//    }
-    
     public var body: some View {
         ScrollView(showsIndicators: showsIndicators) {
-            content()
-                .frame(maxWidth: .infinity)
+            LazyVStack {
+                content()
+                    .frame(maxWidth: .infinity)
+            }
         }
         .background(
             FormBackground()
-//            backgroundColor
                 .edgesIgnoringSafeArea(.all) /// requireds to cover the area that would be covered by the keyboard during its dismissal animation
         )
     }
@@ -45,14 +34,6 @@ public struct FormBackground: View {
     }
 }
 
-//public struct FormBackgroundShapeStyle: ShapeStyle {
-//    @Environment(\.colorScheme) var colorScheme
-//    public init() { }
-//    public var body: some View {
-//        formBackgroundColor(colorScheme: colorScheme)
-//    }
-//}
-
 public struct FormCellBackground: View {
     @Environment(\.colorScheme) var colorScheme
     public init() { }
@@ -60,15 +41,6 @@ public struct FormCellBackground: View {
         formCellBackgroundColor(colorScheme: colorScheme)
     }
 }
-
-//public struct FormCellBackgroundShapeStyle: ShapeStyle {
-//    @Environment(\.colorScheme) var colorScheme
-//    public init() { }
-//    public var body: some View {
-//        formCellBackgroundColor(colorScheme: colorScheme)
-//    }
-//}
-
 
 public func formBackgroundColor(colorScheme: ColorScheme) -> Color {
     return colorScheme == .dark ? Color(hex: "1C1C1E") : Color(.systemGroupedBackground)
