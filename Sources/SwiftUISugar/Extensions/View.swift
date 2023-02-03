20ffb9ccf26602f74f061985d4955ece79240099
Ahmed Khalaf <pxlshpr@gmail.com>
2023-02-03 23:26:56 +0500

    tag increment

--- Sources/SwiftUISugar/Extensions/View.swift
+++ Sources/SwiftUISugar/Extensions/View.swift
@@ -1,6 +1,10 @@
 import SwiftUI
 
-extension View {
+public extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+
     func readSize(onChange: @escaping (CGSize) -> Void) -> some View {
         background(
             GeometryReader { geometryProxy in
@@ -15,4 +19,15 @@ extension View {
 private struct SizePreferenceKey: PreferenceKey {
     static var defaultValue: CGSize = .zero
     static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
+}
+
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
 }
