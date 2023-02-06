e5e94d73a24959b3a23352261b7b66a127948331
Ahmed Khalaf <pxlshpr@gmail.com>
2023-02-06 21:46:36 +0500

    tag increment

---
+++ Sources/SwiftUISugar/Extensions/View+ConfirmedDismissal.swift
@@ -0,0 +1,55 @@
+/// Taken from here: https://gist.github.com/peterfriese/8fb3d76bdbe21b84495b79b3a86bf898
+/// Related links:
+/// https://peterfriese.dev/posts/swiftui-confirmation-dialogs/#confirming-interactive-dismissal
+/// https://twitter.com/peterfriese/status/1464534470820868100?s=20&t=8N6tRIMQDm6nyfX9kFn7IA
+///
+import SwiftUI
+
+extension View {
+    public func interactiveDismissDisabled(_ isDisabled: Bool = true, onAttemptToDismiss: (() -> Void)? = nil) -> some View {
+        InteractiveDismissableView(view: self, isDisabled: isDisabled, onAttemptToDismiss: onAttemptToDismiss)
+    }
+    
+    public func interactiveDismissDisabled(_ isDisabled: Bool = true, attemptToDismiss: Binding<Bool>) -> some View {
+        InteractiveDismissableView(view: self, isDisabled: isDisabled) {
+            attemptToDismiss.wrappedValue.toggle()
+        }
+    }
+    
+}
+
+private struct InteractiveDismissableView<T: View>: UIViewControllerRepresentable {
+    let view: T
+    let isDisabled: Bool
+    let onAttemptToDismiss: (() -> Void)?
+    
+    func makeUIViewController(context: Context) -> UIHostingController<T> {
+        UIHostingController(rootView: view)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIHostingController<T>, context: Context) {
+        context.coordinator.dismissableView = self
+        uiViewController.rootView = view
+        uiViewController.parent?.presentationController?.delegate = context.coordinator
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UIAdaptivePresentationControllerDelegate {
+        var dismissableView: InteractiveDismissableView
+        
+        init(_ dismissableView: InteractiveDismissableView) {
+            self.dismissableView = dismissableView
+        }
+        
+        func presentationControllerShouldDismiss(_ presentationController: UIPresentationController) -> Bool {
+            !dismissableView.isDisabled
+        }
+        
+        func presentationControllerDidAttemptToDismiss(_ presentationController: UIPresentationController) {
+            dismissableView.onAttemptToDismiss?()
+        }
+    }
+}
