0d046a1051d22f8bacb3e0b416f1e09389fb5d8f
Ahmed Khalaf <pxlshpr@gmail.com>
2023-02-13 08:30:48 +0500

    tag increment

--- Sources/SwiftUISugar/Extensions/View.swift
+++ Sources/SwiftUISugar/Extensions/View.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 public extension View {
+    
     func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
         clipShape( RoundedCorner(radius: radius, corners: corners) )
     }
