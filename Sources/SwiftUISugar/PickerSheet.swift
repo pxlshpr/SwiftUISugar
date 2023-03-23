db3ec30600769b655193f3346112a1a43154a763
Ahmed Khalaf <pxlshpr@gmail.com>
2023-03-23 16:12:14 +0500

    bump

--- Sources/SwiftUISugar/PickerSheet.swift
+++ Sources/SwiftUISugar/PickerSheet.swift
@@ -1,25 +1,6 @@
 import SwiftUI
 import SwiftHaptics
 
-public protocol PickableItem: Equatable {
-    var pickerTitle: String { get }
-    var pickerSystemImage: String? { get }
-    var pickerDetail: String? { get }
-}
-
-public extension PickableItem {
-    var pickerSystemImage: String? { nil }
-    var pickerDetail: String? { nil }
-    
-    var pickerItem: PickerItem {
-        PickerItem(
-            id: self.pickerTitle,
-            title: self.pickerTitle,
-            detail: self.pickerDetail,
-            systemImage: self.pickerSystemImage
-        )
-    }
-}
 
 public struct PickerSheet: View {
     
@@ -81,18 +62,39 @@ public struct PickerSheet: View {
                 : Color(.tertiarySystemFill)
             }
             
-            var title: some View {
-                Text(item.title)
-                    .font(.body)
-                    .bold()
-//                    .bold(isSelected)
-                    .foregroundColor(titleColor)
-                    .padding(.vertical, 5)
-                    .padding(.horizontal, 7)
-                    .background(
-                        RoundedRectangle(cornerRadius: 7, style: .continuous)
-                            .foregroundColor(backgroundColor)
+            var linearGradient: LinearGradient? {
+                switch item.colorStyle {
+                case .plain:
+                    return nil
+                case .gradient(let topColor, let bottomColor):
+                    return LinearGradient(
+                        colors: [topColor, bottomColor],
+                        startPoint: .top,
+                        endPoint: .bottom
                     )
+                }
+            }
+            
+            var title: some View {
+                var text: some View {
+                    Text(item.title)
+                        .font(.body)
+                        .bold()
+                        .padding(.vertical, 5)
+                        .padding(.horizontal, 7)
+                        .background(
+                            RoundedRectangle(cornerRadius: 7, style: .continuous)
+                                .foregroundColor(backgroundColor)
+                        )
+                }
+                
+                return Group {
+                    if let linearGradient, !isSelected {
+                        text.foregroundStyle(linearGradient)
+                    } else {
+                        text.foregroundColor(titleColor)
+                    }
+                }
             }
             
             @ViewBuilder
@@ -119,8 +121,13 @@ public struct PickerSheet: View {
             @ViewBuilder
             var image: some View {
                 if let image = item.systemImage {
-                    Image(systemName: image)
-                        .foregroundColor(titleColor)
+                    if let linearGradient, !isSelected {
+                        Image(systemName: image)
+                            .foregroundStyle(linearGradient)
+                    } else {
+                        Image(systemName: image)
+                            .foregroundColor(titleColor)
+                    }
                 }
             }
             
@@ -161,25 +168,43 @@ public struct PickerSheet: View {
     }
 }
 
-public struct PickerItem: Identifiable, Equatable {
+public struct PickerItem: Identifiable {
     
     public var id: String
     public var title: String
     public var detail: String?
     public var secondaryDetail: String?
     public var systemImage: String?
+    public var colorStyle: ColorStyle
+    
+    public enum ColorStyle {
+        case plain
+        case gradient(Color, Color)
+    }
     
     public init(
         id: String? = nil,
         title: String,
         detail: String? = nil,
         secondaryDetail: String? = nil,
-        systemImage: String? = nil
+        systemImage: String? = nil,
+        colorStyle: ColorStyle? = nil
     ) {
         self.id = id ?? UUID().uuidString
         self.title = title
         self.detail = detail
         self.secondaryDetail = secondaryDetail
         self.systemImage = systemImage
+        self.colorStyle = colorStyle ?? .plain
+    }
+}
+
+extension PickerItem: Equatable {
+    public static func ==(lhs: PickerItem, rhs: PickerItem) -> Bool {
+        lhs.id == rhs.id
+        && lhs.title == rhs.title
+        && lhs.detail == rhs.detail
+        && lhs.secondaryDetail == rhs.secondaryDetail
+        && lhs.systemImage == rhs.systemImage
     }
 }
