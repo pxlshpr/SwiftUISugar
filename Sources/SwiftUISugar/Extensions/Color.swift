9e5903cae5bfc3b8371465312ae61bb78d0d99a0
Ahmed Khalaf <pxlshpr@gmail.com>
2023-04-08 19:37:39 +0500

    bump

--- Sources/SwiftUISugar/Extensions/Color.swift
+++ Sources/SwiftUISugar/Extensions/Color.swift
@@ -37,6 +37,46 @@ public extension Color {
 import UIKit
 
 public extension UIColor {
+    convenience init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+
+        self.init(
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            alpha: Double(a) / 255
+        )
+    }
+}
+public extension UIColor {
+    
+    var isLight: Bool {
+        var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+        guard self.getRed(&r, green: &g, blue: &b, alpha: &a) else {
+            return false
+        }
+        
+        /// Taken from: https://stackoverflow.com/questions/2509443/check-if-uicolor-is-dark-or-bright
+        /// which references: http://www.w3.org/WAI/ER/WD-AERT/#color-contrast
+        let brightness = (((r*255)*299) + ((g*255)*587) + ((b*255)*114))/1000.0
+        print("☀️ brightness: \(brightness)")
+        return brightness > 125
+//        return brightness > 230
+    }
+    
     /**
      Create a ligher color
      */
