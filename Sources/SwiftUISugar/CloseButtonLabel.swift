6fe18d734f0d26062da25236898603f4d0ab7d32
Ahmed Khalaf <pxlshpr@gmail.com>
2023-04-30 17:00:56 +0500

    bump

--- Sources/SwiftUISugar/CloseButtonLabel.swift
+++ Sources/SwiftUISugar/CloseButtonLabel.swift
@@ -5,19 +5,42 @@ public struct CloseButtonLabel: View {
     @Environment(\.colorScheme) var colorScheme
     
     let fontSize: CGFloat
-    public init(forUseOutsideOfNavigationBar: Bool = false) {
+    let backgroundStyle: BackgroundStyle
+    
+    public enum BackgroundStyle {
+        case standard
+        case forPlacingOverMaterials
+    }
+    
+    public init(
+        forUseOutsideOfNavigationBar: Bool = false,
+        backgroundStyle: BackgroundStyle = .standard
+    ) {
         self.fontSize = forUseOutsideOfNavigationBar ? 30 : 24
+        self.backgroundStyle = backgroundStyle
+    }
+    
+    var foregroundColor: Color {
+        Color(hex: colorScheme == .light ? "838388" : "A0A0A8")
+    }
+    
+    var backgroundColor: Color {
+        switch backgroundStyle {
+        case .standard:
+            return Color(.quaternaryLabel).opacity(0.5)
+//            return Color(hex: colorScheme == .light ? "EEEEEF" : "313135")
+        case .forPlacingOverMaterials:
+            return colorScheme == .light
+            ? Color(hex: "EEEEEF").opacity(0.5)
+            : Color(.quaternaryLabel).opacity(0.5)
+        }
     }
     
     public var body: some View {
         Image(systemName: "xmark.circle.fill")
             .font(.system(size: fontSize))
             .symbolRenderingMode(.palette)
-            .foregroundStyle(
-                Color(hex: colorScheme == .light ? "838388" : "A0A0A8"),      /// 'x' symbol
-//                Color(hex: colorScheme == .light ? "EEEEEF" : "313135") /// background
-                Color(.quaternaryLabel).opacity(0.5)
-            )
+            .foregroundStyle(foregroundColor, backgroundColor)
     }
     
 }
