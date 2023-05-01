a54f369e0190390a2ead760feb64348f8a8d4fc2
Ahmed Khalaf <pxlshpr@gmail.com>
2023-05-01 13:46:00 +0500

    bump

--- Sources/SwiftUISugar/CloseButtonLabel.swift
+++ Sources/SwiftUISugar/CloseButtonLabel.swift
@@ -1,23 +1,28 @@
 import SwiftUI
 
-public struct CloseButtonLabel: View {
+public struct TopButtonLabel: View {
     
     @Environment(\.colorScheme) var colorScheme
     
     let fontSize: CGFloat
     let backgroundStyle: BackgroundStyle
-    
-    public enum BackgroundStyle {
-        case standard
-        case forPlacingOverMaterials
-    }
+    let systemImage: String
     
     public init(
+        systemImage: String,
         forUseOutsideOfNavigationBar: Bool = false,
         backgroundStyle: BackgroundStyle = .standard
     ) {
         self.fontSize = forUseOutsideOfNavigationBar ? 30 : 24
         self.backgroundStyle = backgroundStyle
+        self.systemImage = systemImage
+    }
+    
+    public var body: some View {
+        Image(systemName: systemImage)
+            .font(.system(size: fontSize))
+            .symbolRenderingMode(.palette)
+            .foregroundStyle(foregroundColor, backgroundColor)
     }
     
     var foregroundColor: Color {
@@ -36,13 +41,32 @@ public struct CloseButtonLabel: View {
         }
     }
     
-    public var body: some View {
-        Image(systemName: "xmark.circle.fill")
-            .font(.system(size: fontSize))
-            .symbolRenderingMode(.palette)
-            .foregroundStyle(foregroundColor, backgroundColor)
+    public enum BackgroundStyle {
+        case standard
+        case forPlacingOverMaterials
+    }
+}
+
+public struct CloseButtonLabel: View {
+    
+    let backgroundStyle: TopButtonLabel.BackgroundStyle
+    let forUseOutsideOfNavigationBar: Bool
+    
+    public init(
+        forUseOutsideOfNavigationBar: Bool = false,
+        backgroundStyle: TopButtonLabel.BackgroundStyle = .standard
+    ) {
+        self.forUseOutsideOfNavigationBar = forUseOutsideOfNavigationBar
+        self.backgroundStyle = backgroundStyle
     }
     
+    public var body: some View {
+        TopButtonLabel(
+            systemImage: "xmark.circle.fill",
+            forUseOutsideOfNavigationBar: forUseOutsideOfNavigationBar,
+            backgroundStyle: backgroundStyle
+        )
+    }
 }
 
 public var navigationLinkArrow: some View {
